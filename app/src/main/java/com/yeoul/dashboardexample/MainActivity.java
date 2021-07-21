e843cb8a8da2606d15717b6c19da8824a5b82b5c
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2021-07-21 23:58:12 +0900

    add splash activity

--- app/src/main/java/com/yeoul/dashboardexample/MainActivity.java
+++ app/src/main/java/com/yeoul/dashboardexample/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,18 +25,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        /* ----- Hooks ----- */
         drawerLayout = findViewById(R.id.drawerlayout_main);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
+        /* ----- Toolbar ----- */
         setSupportActionBar(toolbar);
 
+        /* ----- Navibation Drawer Menu ----- */
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
         navigationView.bringToFront();
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_g1_home);
+
+        // TODO : set this option by Keep Login Options
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_g2_profile).setVisible(false);
+        menu.findItem(R.id.nav_g2_logout).setVisible(false);
     }
 
     @Override
@@ -52,7 +63,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+        switch (item.getItemId()) {
+            case R.id.nav_g1_home:
+            case R.id.nav_g1_example2:
+            case R.id.nav_g1_example1:
+            case R.id.nav_g1_example3:
+            case R.id.nav_g2_profile:
+            case R.id.nav_g2_login:
+            case R.id.nav_g2_logout:
+                toast_not_implemented();
+                break;
+        }
 
+        drawerLayout.closeDrawer(GravityCompat.START);
         return false;
+    }
+
+    private void toast_not_implemented() {
+        Toast.makeText(this, "Not implmented", Toast.LENGTH_LONG).show();
+        return;
     }
 }
